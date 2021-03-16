567909fde19342a6c305f6c3f8bd28c4a155487f
Daniel-Melo97 <daniellemosmelo@hotmail.com>
2021-03-16 13:17:53 -0300

    Organização de pastas inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { MaterialModule } from './material.module';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { Page2Component } from './pages/page2/page2.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+const routes: Routes = [{path:'', redirectTo: '/main', pathMatch: 'full' },
+                        {path:'main', component: MainPageComponent },
+                        {path:'**', redirectTo: '/notFound'}];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    Page2Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
